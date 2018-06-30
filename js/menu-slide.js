10121a31dd76221ebc45e1592d8b432b6e0b55ae
Josué Martín <josue@matrushka.com.mx>
2018-06-29 19:31:24 -0500

    Adding menu slide script

---
+++ js/menu-slide.js
@@ -0,0 +1,29 @@
+$(function() {
+  var $menuSlideTriggers = $('.menu-slide-trigger');
+
+  function getMenuSlideId($menuTrigger) {
+    var menuId =  "menu_" + $menuTrigger.data('menu-id');
+    return menuId;
+  }
+
+  function toogleMenuSlide(menuSlideId) {
+    $menuSlideSelected = $('#' + menuSlideId);
+    $menuSlideSelected.siblings().removeClass('is-open');
+    $menuSlideSelected.toggleClass('is-open');
+  }
+
+  function toogleMenuIndicator(menuSlideId) {
+    $menuIndicatorSelected = $('#menuItem_' + menuSlideId);
+    $menuIndicatorSelected.siblings().removeClass('is-selected');
+    $menuIndicatorSelected.toggleClass('is-selected');
+  }
+
+  $menuSlideTriggers.click(function(ev){
+    ev.preventDefault();
+    menuSlideId = getMenuSlideId($(ev.currentTarget));
+    if(menuSlideId != "menu_main") {
+      toogleMenuIndicator(menuSlideId)
+    }
+    toogleMenuSlide(menuSlideId)
+  })
+});
